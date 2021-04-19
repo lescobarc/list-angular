898f332cfbc50fafe7b52a39aa21572739d0444c
lauraescobarc <66136705+lescobarc@users.noreply.github.com>
2021-04-19 13:16:12 -0500

    lista-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ItemsComponent } from './components/items/items.component';
+import { ItemComponent } from './components/item/item.component';
+import { TotalComponent } from './components/total/total.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ItemsComponent,
+    ItemComponent,
+    TotalComponent,
+    
   ],
   imports: [
     BrowserModule,
